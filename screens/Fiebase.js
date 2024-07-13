aea417d77bc7fe92d7e9235109907a1926ecfd08
tomer <sarusi@mail.tau.ac.il>
2024-07-13 17:37:39 +0300

    Initial Firebase Implementation

---
+++ screens/Fiebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABWcyPdbh9dDautY3BjaL4FJQY94-at5E",
+    authDomain: "workshop-23878.firebaseapp.com",
+    projectId: "workshop-23878",
+    storageBucket: "workshop-23878.appspot.com",
+    messagingSenderId: "995336065754",
+    appId: "1:995336065754:web:5da21b999a1502a8668be3",
+    measurementId: "G-KL9HH4BFXT"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
