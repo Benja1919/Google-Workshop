0b847d46d80a2dce18858e2537c2e72e411c63dd
Ben Cohen <bc1@mail.tau.ac.il>
2024-07-20 15:49:08 +0300

    started working on search

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from './AuthContext';
 import { View, Text, TextInput, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native'; // Import navigation hook
+import { useNavigation } from '@react-navigation/native';
 import BottomBarComponent from './components/BottomBar';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 import { firestoreDB } from './FirebaseDB';
 
 const SearchScreen = () => {
-  const { isLoggedIn} = useContext(AuthContext);
+  const { isLoggedIn } = useContext(AuthContext);
   const onGestureEvent = (event) => {
     if (event.nativeEvent.translationX < -100) {
-      if(isLoggedIn){
+      if (isLoggedIn) {
         navigation.navigate('PostCreation');
-      }
-      else{
+      } else {
         navigation.navigate('LoginScreen');
       }
-    }
-    else if (event.nativeEvent.translationX > 100) {
+    } else if (event.nativeEvent.translationX > 100) {
       navigation.navigate('HomeScreen');
     }
   };
@@ -25,17 +23,16 @@ const SearchScreen = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [isUserSearch, setIsUserSearch] = useState(false);
   const [recentSearches, setRecentSearches] = useState([]);
-  const navigation = useNavigation(); // Initialize navigation hook
-  const db = firestoreDB(); // Initialize the mock database instance
+  const navigation = useNavigation();
+  const db = firestoreDB();
 
-
-  // Function to handle search
-  const handleSearch = (query) => {
-    const users = Object.values(db.GetUsers());
-    const posts = db.GetPosts();
+  // Handle the search logic for users and posts
+  const handleSearch = async (query) => {
+    const users = await db.GetUsers();
+    const posts = await db.GetPosts();
 
     const filteredUsers = users.filter(user =>
-      user.userName.toLowerCase().includes(query.toLowerCase())
+      user.userName && user.userName.toLowerCase().includes(query.toLowerCase())
     );
 
     if (filteredUsers.length > 0) {
@@ -43,26 +40,24 @@ const SearchScreen = () => {
       setIsUserSearch(true);
     } else {
       const filteredPosts = posts.filter(post =>
-        post.restaurantName.toLowerCase().includes(query.toLowerCase()) ||
-        post.content.toLowerCase().includes(query.toLowerCase())
+        (post.restaurantName && post.restaurantName.toLowerCase().includes(query.toLowerCase())) ||
+        (post.content && post.content.toLowerCase().includes(query.toLowerCase()))
       );
       setSearchResults(filteredPosts);
       setIsUserSearch(false);
     }
 
-    // Update recent searches
     updateRecentSearches(query);
   };
 
-  // Function to update recent searches
+  // Update recent searches with the new query
   const updateRecentSearches = (query) => {
     if (query && !recentSearches.includes(query)) {
-      const updatedSearches = [query, ...recentSearches.slice(0, 4)]; // Add query to the beginning and limit to 5 recent searches
+      const updatedSearches = [query, ...recentSearches.slice(0, 4)];
       setRecentSearches(updatedSearches);
     }
   };
 
-  // Load recent searches on component mount
   useEffect(() => {
     // Load recent searches from storage or initialize if empty
     // Example using AsyncStorage:
@@ -80,12 +75,12 @@ const SearchScreen = () => {
   };
 
   const handleUserPress = (userName) => {
-
     navigation.navigate('UserProfile', { userName });
   };
 
-  const handlePostPress = (postId) => {
-    navigation.navigate('Post', { postId });
+  const handleCategoryPress = (category) => {
+    setSearchQuery(category);
+    handleSearch(category);
   };
 
   const renderResultItem = ({ item }) => {
@@ -105,6 +100,8 @@ const SearchScreen = () => {
         <TouchableOpacity onPress={() => handlePostPress(item.id)}>
           <View style={styles.postItem}>
             <Image source={{ uri: item.imageUrl }} style={styles.postImage} />
+            <Text>{item.restaurantName}</Text>
+            <Image source={{ uri: item.profileImageUrl }} style={styles.profileImage} />
             <Text>{item.userName}</Text>
             <Text>{item.content}</Text>
           </View>
@@ -131,77 +128,68 @@ const SearchScreen = () => {
   );
 
   return (
-    <PanGestureHandler
-      onGestureEvent={onGestureEvent}
-    >
-    <View style={{ flex: 1, padding: 20 }}>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('fire')}>
-          <View>
-            <Image source={require('../assets/icons/fire.png')} style={styles.searchicon} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('burger')}>
-          <View>
-            <Image source={require('../assets/icons/burger.png')} style={styles.searchicon} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('pizza')}>
-          <View>
-            <Image source={require('../assets/icons/pizza.png')} style={styles.searchicon} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('sushi')}>
-          <View>
-            <Image source={require('../assets/icons/sushi.png')} style={styles.searchicon} />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('cake')}>
-          <View>
-            <Image source={require('../assets/icons/cake.png')} style={styles.searchicon} />
-          </View>
-        </TouchableOpacity>
-      </View>
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <View style={{ flex: 1, padding: 20 }}>
+        <View style={styles.buttonContainer}>
+          {/* <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('fire')}>
+            <View>
+              <Image source={require('../assets/icons/fire.png')} style={styles.searchicon} />
+            </View>
+          </TouchableOpacity> */}
+          <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('burger')}>
+            <View>
+              <Image source={require('../assets/icons/burger.png')} style={styles.searchicon} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('pizza')}>
+            <View>
+              <Image source={require('../assets/icons/pizza.png')} style={styles.searchicon} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('sushi')}>
+            <View>
+              <Image source={require('../assets/icons/sushi.png')} style={styles.searchicon} />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.categoryButton} onPress={() => handleCategoryPress('cake')}>
+            <View>
+              <Image source={require('../assets/icons/cake.png')} style={styles.searchicon} />
+            </View>
+          </TouchableOpacity>
+        </View>
 
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Enter search query"
-          value={searchQuery}
-          onChangeText={text => setSearchQuery(text)}
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Enter search query"
+            value={searchQuery}
+            onChangeText={text => setSearchQuery(text)}
+          />
+          <TouchableOpacity style={styles.searchButton} onPress={() => handleSearch(searchQuery)}>
+            <Image source={require('../assets/icons/search.png')} style={styles.searchbarIcon} />
+          </TouchableOpacity>
+        </View>
+
+        {searchResults.length === 0 && recentSearches.length > 0 && renderRecentSearches()}
+
+        {searchResults.length === 0 && recentSearches.length === 0 && renderNoResults()}
+
+        <FlatList
+          style={{ marginTop: 20 }}
+          data={searchResults}
+          keyExtractor={(item) => item.id ? item.id.toString() : item.userName}
+          renderItem={renderResultItem}
         />
-        <TouchableOpacity
-          style={styles.searchButton}
-          onPress={() => handleSearch(searchQuery)}
-        >
-          <Image source={require('../assets/icons/search.png')} style={styles.searchbarIcon} />
-        </TouchableOpacity>
+
+        <View style={styles.Pusher} />
+        <BottomBarComponent navigation={navigation} />
       </View>
-      
-      {searchResults.length === 0 && recentSearches.length > 0 && (
-        renderRecentSearches()
-      )}
-
-      {searchResults.length === 0 && recentSearches.length === 0 && (
-        renderNoResults()
-      )}
-
-      <FlatList
-        style={{ marginTop: 20 }}
-        data={searchResults}
-        keyExtractor={(item) => item.id ? item.id.toString() : item.userName}
-        renderItem={renderResultItem}
-      />
-
-      <View Push style={styles.Pusher}/>
-      <BottomBarComponent navigation={navigation}/>
-    </View>
     </PanGestureHandler>
   );
 };
 
 const styles = StyleSheet.create({
-  Pusher:{
+  Pusher: {
     flex: 1,
   },
   buttonContainer: {
@@ -238,8 +226,8 @@ const styles = StyleSheet.create({
     height: 20,
   },
   searchicon: {
-    width: 30,
-    height: 30,
+    width: 50,
+    height: 40,
   },
   bottomBar: {
     flexDirection: 'row',
@@ -259,35 +247,37 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     borderRadius: 8,
   },
-  icon: {
-    width: 25,
-    height: 25,
+  bottomBarButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
   userItem: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
+    paddingVertical: 10,
   },
   profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
     marginRight: 10,
   },
   postItem: {
-    marginBottom: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
   },
   postImage: {
-    width: '100%',
-    height: 200,
-    marginBottom: 10,
+    width: 200,
+    height: 100,
+    marginBottom: 5,
   },
   noResultsContainer: {
     alignItems: 'center',
     marginTop: 20,
   },
   noResultsText: {
-    fontSize: 18,
+    fontSize: 16,
     color: 'gray',
   },
   recentSearchesContainer: {
@@ -299,9 +289,9 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   recentSearch: {
-    fontSize: 14,
-    color: 'blue',
-    marginBottom: 5,
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
   },
 });
 
